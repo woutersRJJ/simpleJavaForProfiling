ef3b0ca1d280b292e343c79412b51db4fee607af
Ronny Wouters <ozzy.wouters@telenet.be>
2022-03-24 16:08:00 +0100

    int[50 mio] i.p.v. ArrayList<Integer>`

--- src/main/java/Exercise01/RandomNumbers.java
+++ src/main/java/Exercise01/RandomNumbers.java
@@ -1,6 +1,7 @@
 package Exercise01;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RandomNumbers {
@@ -8,18 +9,21 @@ public class RandomNumbers {
 
     public void go() {
         while (true) {
-            ArrayList<Integer> arrayOfRandomNumbers = getArrayOfRandomNumbers();
+            int[] arrayOfRandomNumbers = getArrayOfRandomNumbers();
         }
     }
 
-    private ArrayList<Integer> getArrayOfRandomNumbers() {
-        ArrayList<Integer> numbers = new ArrayList<>();
+    private int[] getArrayOfRandomNumbers() {
+        //int[] numbers = new int[Integer.MAX_VALUE-1];
+        int[] numbers = new int[50000000];
+
         System.out.println("how many numbers in this array? ");
         int length = sc.nextInt();
         for (int i = 0; i < length; i++) {
-            numbers.add((int) (Math.random() * 100));
+            numbers[i] = (int) (Math.random() * 100);
         }
         //System.out.println("new array: "+ numbers);
+        System.out.println(Arrays.toString(numbers));
         return numbers;
     }
 
