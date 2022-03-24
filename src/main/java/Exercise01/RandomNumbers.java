b6ed7101fe7c223e31154336536684d4197dd2ff
Ronny Wouters <ozzy.wouters@telenet.be>
2022-03-24 19:47:42 +0100

    Ex 3.8 int i.p.v. Integer met en zonder getallen printen

--- src/main/java/Exercise01/RandomNumbers.java
+++ src/main/java/Exercise01/RandomNumbers.java
@@ -8,27 +8,27 @@ public class RandomNumbers {
 
     public void go() {
         while (true) {
-            ArrayList<Integer> arrayOfRandomNumbers = getArrayOfRandomNumbers();
+            int[] arrayOfRandomNumbers = getArrayOfRandomNumbers();
         }
     }
 
-    private ArrayList<Integer> getArrayOfRandomNumbers() {
-        ArrayList<Integer> numbers = new ArrayList<>();
+    private int[] getArrayOfRandomNumbers() {
+        int[] numbers = new int[10000000];
         System.out.println("how many numbers in this array? ");
         int length = sc.nextInt();
         for (int i = 0; i < length; i++) {
-            numbers.add((int) (Math.random() * 100));
+            numbers[i] = (int) (Math.random() * 100);
         }
-       /* printArrayV1(numbers);
-        printArrayV2(numbers);*/
+        printArrayV1(numbers);
+        printArrayV2(numbers);
         return numbers;
     }
 
-    private void printArrayV1(ArrayList<Integer> numbers) {
+    private void printArrayV1(int[] numbers) {
         System.out.println("new array: "+ numbers);
     }
 
-    private void printArrayV2(ArrayList<Integer> numbers) {
+    private void printArrayV2(int[] numbers) {
         for (int number: numbers)
             System.out.print(number+", ");
         System.out.println("");
