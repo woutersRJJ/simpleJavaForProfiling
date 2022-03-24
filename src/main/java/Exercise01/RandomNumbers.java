75f7fa6251e965e1c464a6b93ffd13c7c9f53de8
Ronny Wouters <ozzy.wouters@telenet.be>
2022-03-24 17:13:17 +0100

    Ex 3.5 int i.p.v. Integer zonder getallen printen`

--- src/main/java/Exercise01/RandomNumbers.java
+++ src/main/java/Exercise01/RandomNumbers.java
@@ -4,25 +4,27 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RandomNumbers {
-    ArrayList<ArrayList<Integer>> listOfAllArrays = new ArrayList<>();
+    ArrayList<int[]> arrayOfAllArrays = new ArrayList<int[]>();
     Scanner sc = new Scanner(System.in);
 
     public void go() {
         while (true) {
-            ArrayList<Integer> arrayOfRandomNumbers = getArrayOfRandomNumbers();
-            listOfAllArrays.add(arrayOfRandomNumbers);
+            int[] arrayOfRandomNumbers = getArrayOfRandomNumbers();
+
+            arrayOfAllArrays.add(arrayOfRandomNumbers);
             //System.out.println("list of all arrays: "+ listOfAllArrays);
         }
     }
 
-    private ArrayList<Integer> getArrayOfRandomNumbers() {
-        ArrayList<Integer> numbers = new ArrayList<>();
+    private int[] getArrayOfRandomNumbers() {
+        //ArrayList<Integer> numbers = new ArrayList<>();
+        int[]numbers=new int[50000000];
         System.out.println("how many numbers in this array? ");
         int length = sc.nextInt();
         for (int i = 0; i < length; i++) {
-            numbers.add((int) (Math.random() * 100));
+            numbers[i] = (int) (Math.random() * 100);
         }
-        System.out.println("new array: "+ numbers);
+        //System.out.println("new array: "+ numbers);
         return numbers;
     }
 
